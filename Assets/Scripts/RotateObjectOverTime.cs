e0a935828c4e28c4acbdaafe36c4cf0a777901e8
ThunderAwesome <troyjsanders@gmail.com>
2019-10-07 00:27:11 -0500

    Separated out the rotation of the bell to its own script

---
+++ Assets/Scripts/RotateObjectOverTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RotateObjectOverTime : MonoBehaviour
+{
+    [SerializeField]
+    private float m_rotateSpeed = 100f;
+
+    [SerializeField]
+    private float m_oscillateIntensity = 0.2f;
+
+    private Vector3 m_startPosition = Vector3.zero;
+
+    private void Update()
+    {
+        transform.Rotate(0f, Time.deltaTime * m_rotateSpeed, 0f, Space.Self);
+        transform.localPosition = m_startPosition + new Vector3(0f, Mathf.Sin(Time.time) * m_oscillateIntensity, 0f);
+    }
+}
